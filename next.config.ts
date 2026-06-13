ed827209bb9796243b5d7854d7987b5c209237e3
vinaypropy <vinay.propy@gmail.com>
2026-06-13 13:46:56 +0530

    fix: add images.unsplash.com to remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'dslwgbtaxwyotsyjqwhd.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
